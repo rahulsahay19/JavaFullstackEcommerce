e2022b650767e63a6d1ff345b328b8b5deac16ac
rahulsahay19 <rahulsahay19@hotmail.com>
2023-12-23 20:09:34 +0530

    initial commit

---
+++ src/main/java/com/ecoomerce/sportscenter/SportscenterApplication.java
@@ -0,0 +1,13 @@
+package com.ecoomerce.sportscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportscenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportscenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoomerce/sportscenter/SportscenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoomerce.sportscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportscenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
