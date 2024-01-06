63b75d5bb5d12c20ffb792cae639461409ba2576
rahulsahay19 <rahulsahay19@hotmail.com>
2024-01-07 00:22:44 +0530

    fixed breadcrumb issue

--- client/src/app/account/account-routing.module.ts
+++ client/src/app/account/account-routing.module.ts
@@ -5,8 +5,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent, data:{breadcrumb: 'Login'}},
-  {path: 'register', component: RegisterComponent, data:{breadcrumb: 'Register'}}
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ]
 
 @NgModule({
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 
 const routes: Routes = [
   {path:'', component: HomeComponent, data:{breadcrumb: 'Home'}},
-  {path:'store', loadChildren:()=>import('./store/store.module').then(m=>m.StoreModule), data:{breadcrumb: 'Store'}},
-  {path:'basket', loadChildren:()=>import('./basket/basket.module').then(m=>m.BasketModule), data:{breadcrumb: 'Basket'}},
-  {path:'account', loadChildren:()=>import('./account/account.module').then(m=>m.AccountModule), data:{breadcrumb: 'Account'}},
+  {path:'store', loadChildren:()=>import('./store/store.module').then(m=>m.StoreModule)},
+  {path:'basket', loadChildren:()=>import('./basket/basket.module').then(m=>m.BasketModule)},
+  {path:'account', loadChildren:()=>import('./account/account.module').then(m=>m.AccountModule)},
   {path:'not-found', component: NotFoundComponent},
   {path:'server-error', component: ServerErrorComponent},
   {path:'**', redirectTo: '', pathMatch:'full'}
